621ea0e83677094c2a3e12531665b5c69103fb6d
Maneesh Mehra <maneeshmehra@yahoo.com>
2020-09-18 11:56:33 -0400

    Add project to source control

---
+++ constats/src/test/java/com/rh/constats/ConstatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rh.constats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConstatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
